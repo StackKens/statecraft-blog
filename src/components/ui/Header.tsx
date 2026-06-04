9163dbd9baa1de3cd84369aeaa203fd60ab0bf7b
StackKens <ofwonoalex256@gmail.com>
2026-06-04 16:05:55 +0300

    refactor: add white background to header component to give a more visible header contents on page scroll

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,16 +2,16 @@ import { NavbarItem } from "../../config/navbar";
 
 export default function Welcome() {
   return (
-    <div className="items-center justify-between py-6 px-4 md:px-8 lg:px-16 h-16 flex sticky top-0 z-50 w-full shadow-md cursor-pointer">
+    <div className="items-center justify-between py-6 px-4 md:px-8 lg:px-16 h-16 flex sticky top-0 z-50 w-full shadow-md cursor-pointer bg-white/80 backdrop-blur-md">
       <div>
         <p className="text-2xl font-bold tracking-tight text-emerald-700">
           StateCraftBlog
         </p>
       </div>
 
-      <div className="flex px-3 gap-6 text-sm font-medium text-gray-700 ">
+      <div className="flex px-3 gap-6 text-sm font-medium text-gray-700">
         {NavbarItem.map((item) => (
-          <nav className=" hover:text-emerald-700 " key={item.label}>
+          <nav className="hover:text-emerald-700" key={item.label}>
             {item.label}
           </nav>
         ))}
