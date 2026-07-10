3d172c2681b7034e249e7db8f6f73da4b4af26ae
StackKens <ofwonoalex256@gmail.com>
2026-07-10 11:21:04 +0300

    feat: create users sql to create the table users in the database

--- frontend/src/services/postService.ts
+++ frontend/src/services/postService.ts
@@ -1,11 +1,10 @@
 import type { Post } from "../types/post";
 
 export async function fetchPosts(): Promise<Post[]> {
-  const res = await fetch("http://localhost:3001/posts");
+  const res = await fetch("http://localhost:3000/posts");
   if (!res.ok) {
     throw new Error(`failed to fetch ${res.status}`);
   }
-
   const data: Post[] = await res.json();
   return data;
 }
