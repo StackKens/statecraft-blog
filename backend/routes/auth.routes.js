f40052bfc5f66a02383dbbead73aad4ae228d6ba
StackKens <ofwonoalex256@gmail.com>
2026-07-14 12:13:05 +0300

    feat: mount register route in server

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const login = require("../controllers/auth.controller");
+const { login, register } = require("../controllers/auth.controller");
 
 router.post("/login", login);
+router.post("/register", register);
 module.exports = router;
