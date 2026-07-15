2b8b38b3dc5275633d6624dfbfc6e6994669f277
StackKens <ofwonoalex256@gmail.com>
2026-07-15 17:14:27 +0300

    feat: add CRUD endpoints for posts with ownership checks

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { getAllPosts, getPostById } = require("../controllers/post.controller");
+const {
+  getAllPosts,
+  getPostById,
+  getUserPosts,
+  createPost,
+  updatePost,
+  deletePost,
+} = require("../controllers/post.controller");
+const authenticate = require("../middleware/auth.middleware");
+
 router.get("/", getAllPosts);
+router.get("/me", authenticate, getUserPosts);
 router.get("/:id", getPostById);
+router.post("/", authenticate, createPost);
+router.put("/:id", authenticate, updatePost);
+router.delete("/:id", authenticate, deletePost);
+
 module.exports = router;
