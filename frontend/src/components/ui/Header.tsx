4a4bd3119efbc6f9e2839ac9db61b71320ef0a60
StackKens <ofwonoalex256@gmail.com>
2026-07-15 16:39:11 +0300

    fix: refactor the authcontext to fail loudly, instead of console.log

--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -1,17 +1,21 @@
 import { NavbarItem } from "../../config/navbar";
 import { useTheme } from "../../context/ThemeContext";
+import { useAuth } from "../../context/AuthConext";
 
 interface HeaderProps {
-  setModal: React.Dispatch<React.SetStateAction<"login" | "register" | null>>;
+  setModal: React.Dispatch<
+    React.SetStateAction<"login" | "register" | "createPost" | null>
+  >;
 }
 
 export default function Header({ setModal }: HeaderProps) {
   const { theme, toggleTheme } = useTheme();
+  const { isAuthenticated } = useAuth();
 
   return (
     <div
       className={`items-center justify-between py-6 px-4 md:px-8 lg:px-16 h-16 flex sticky top-0 z-50 w-full shadow-md backdrop-blur-md ${
-        theme === "dark" ? "bg-gray-950 text-white" : "bg-white/80"
+        theme === "dark" ? "bg-gray-900/80" : "bg-white/80"
       }`}
     >
       <p className="text-2xl font-bold tracking-tight text-emerald-700">
@@ -19,17 +23,26 @@ export default function Header({ setModal }: HeaderProps) {
       </p>
 
       <div className="flex px-3 gap-6 text-sm font-medium text-gray-700">
-        {NavbarItem.map((item) => (
+        {isAuthenticated ? (
           <button
-            key={item.label}
-            className="cursor-pointer hover:text-emerald-700"
-            onClick={() =>
-              setModal(item.link === "/login" ? "login" : "register")
-            }
+            className="cursor-pointer text-emerald-700 hover:text-emerald-900 font-semibold"
+            onClick={() => setModal("createPost")}
           >
-            {item.label}
+            + New Post
           </button>
-        ))}
+        ) : (
+          NavbarItem.map((item) => (
+            <button
+              key={item.label}
+              className="cursor-pointer hover:text-emerald-700"
+              onClick={() =>
+                setModal(item.link === "/login" ? "login" : "register")
+              }
+            >
+              {item.label}
+            </button>
+          ))
+        )}
       </div>
 
       <button
